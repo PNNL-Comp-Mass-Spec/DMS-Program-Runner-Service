be3032caab1eee6a6e517a4ecb4dc234c53fa736
Matthew Monroe <matthew.monroe@pnnl.gov>
2006-06-30 01:36:56 +0000

    ProgRunner_Svc_Net

---
+++ AssemblyInfo.vb
@@ -0,0 +1,31 @@
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Program Runner Service")> 
+<Assembly: AssemblyDescription("Run an arbitrary set of programs as a service")> 
+<Assembly: AssemblyCompany("Pacific Northwest National Laboratory")> 
+<Assembly: AssemblyProduct("Program Runner Service")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("E1B5D3EE-2D4D-4F68-B37D-A053F36120F3")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")> 
