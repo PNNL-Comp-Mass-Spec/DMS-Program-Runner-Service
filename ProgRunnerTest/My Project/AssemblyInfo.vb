77bcb694f8482c5f5812144ca3a907b5a93d7d4f
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-12 16:40:29 -0700

    Update to .NET 4.6.2 and obtain log4net from NuGet

--- ProgRunnerTest/My Project/AssemblyInfo.vb
+++ ProgRunnerTest/My Project/AssemblyInfo.vb
@@ -2,34 +2,33 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("ProgRunnerTest")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Battelle PNNL")> 
-<Assembly: AssemblyProduct("ProgRunnerTest")> 
-<Assembly: AssemblyCopyright("Copyright © Battelle PNNL 2009")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("ProgRunnerTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Pacific Northwest National Laboratory")>
+<Assembly: AssemblyProduct("ProgRunnerTest")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("276103fe-c9ba-4d2e-be4b-c58e01bddcc1")> 
+<Assembly: Guid("276103fe-c9ba-4d2e-be4b-c58e01bddcc1")>
 
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.*")>
