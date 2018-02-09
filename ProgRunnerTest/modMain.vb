e1842a50d5fdb17eea211a3c69f24adfe6408ac1
alchemistmatt <matthew.monroe@pnnl.gov>
2018-02-09 13:27:03 -0800

    Update PRISM.dll from NuGet

--- ProgRunnerTest/modMain.vb
+++ ProgRunnerTest/modMain.vb
@@ -2,24 +2,23 @@
 
 Imports System.Threading
 
-
 Module modMain
 
     Sub Main()
         Dim myProgRunner As New ProgRunnerSvc.clsMainProg()
 
-        'clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Start")
+        ' FileLogger.WriteLog(BaseLogger.LogLevels.INFO, "Start")
 
-        'Start the service running
+        ' Start the service running
         myProgRunner.StartAllProgRunners()
 
         ' Wait for 15 seconds
         Thread.Sleep(15 * 1000)
 
-        'Stop the service
+        ' Stop the service
         myProgRunner.StopAllProgRunners()
 
-        'clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Stop")
+        ' FileLogger.WriteLog(BaseLogger.LogLevels.INFO, "Stop")
 
     End Sub
 
