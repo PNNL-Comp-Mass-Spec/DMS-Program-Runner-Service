6fad515ce2eec9dca87e4de82af07b09a5f063dd
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-15 11:59:59 -0700

    Update automation files

--- Automated_Scripts/CopyProgrunnerWork.bat
+++ Automated_Scripts/CopyProgrunnerWork.bat
@@ -1,12 +1,21 @@
-if exist %1 (goto StartWork)
+set FullPath=\\%1\DMS_Programs\MultiProgRunnerSvc
+
+if exist %FullPath% (goto StartWork)
 
 echo.
-echo Path not found: %1
+echo Path not found: %FullPath%
 goto Done
 
 :StartWork
 
-xcopy \\Proto-2\past\Software\MultiProgRunnerSvc\Bin\*.exe %1 /D /Y
-xcopy \\Proto-2\past\Software\MultiProgRunnerSvc\Bin\*.dll %1 /D /Y
+echo.
+echo Updating %FullPath%
+
+if exist %FullPath%\logging.config del %FullPath%\logging.config
+if exist %FullPath%\log4net.dll del %FullPath%\log4net.dll
+
+xcopy \\Proto-2\past\Software\MultiProgRunnerSvc\Bin\ProgRunnerSvc.exe %FullPath% /D /Y
+xcopy \\Proto-2\past\Software\MultiProgRunnerSvc\Bin\PRISM.dll %FullPath% /D /Y
+xcopy \\Proto-2\past\Software\MultiProgRunnerSvc\Bin\README.md %FullPath% /D /Y
 
 :Done
--- Automated_Scripts/CopyProgrunner_AllMachines.bat
+++ Automated_Scripts/CopyProgrunner_AllMachines.bat
@@ -1,118 +1,143 @@
-rem call CopyProgrunnerWork.bat \\peaks1\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\peaks2\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-02\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-03\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-04\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-05\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-06\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-07\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-08\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-09\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-10\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-11\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-12\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-13\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-14\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-15\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-16\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-17\DMS_Programs\MultiProgRunnerSvc\
+:Protos
+echo Update Protos
+call CopyProgrunnerWork.bat Proto-2
+call CopyProgrunnerWork.bat Proto-3
+call CopyProgrunnerWork.bat Proto-4
+call CopyProgrunnerWork.bat Proto-5
+call CopyProgrunnerWork.bat Proto-6
+call CopyProgrunnerWork.bat Proto-7
+rem Offline: Proto-8
+rem Offline: Proto-9
+rem Offline: Proto-10
+call CopyProgrunnerWork.bat Proto-11
 
-rem call CopyProgrunnerWork.bat \\Pub-24\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-25\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-26\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-27\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-28\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-29\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-30\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-31\DMS_Programs\MultiProgRunnerSvc\
+:Pubs
+echo Update Pub-0*
+call CopyProgrunnerWork.bat Pub-02
+call CopyProgrunnerWork.bat Pub-03
+call CopyProgrunnerWork.bat Pub-04
+call CopyProgrunnerWork.bat Pub-05
+call CopyProgrunnerWork.bat Pub-06
+call CopyProgrunnerWork.bat Pub-07
+call CopyProgrunnerWork.bat Pub-08
+call CopyProgrunnerWork.bat Pub-09
 
-call CopyProgrunnerWork.bat \\Pub-32\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-33\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-34\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-35\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-36\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-37\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-38\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-39\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-40\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\Pub-41\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-42\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-43\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-44\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-45\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-46\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-47\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-48\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-49\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-50\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-51\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-52\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-53\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-54\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-55\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-56\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-57\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-58\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-59\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-60\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-61\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-62\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-63\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-64\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-65\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-66\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-67\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-68\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-69\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-70\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-71\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-72\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-73\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-74\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-75\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-76\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-77\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-78\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-79\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-80\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-81\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-82\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-83\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-84\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-85\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-86\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-87\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-88\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-89\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-90\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-91\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-92\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-93\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-94\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-95\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-96\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Pub-97\DMS_Programs\MultiProgRunnerSvc\
+echo Update Pub-1*
+call CopyProgrunnerWork.bat Pub-10
+call CopyProgrunnerWork.bat Pub-11
+call CopyProgrunnerWork.bat Pub-12
+call CopyProgrunnerWork.bat Pub-13
+call CopyProgrunnerWork.bat Pub-14
+call CopyProgrunnerWork.bat Pub-15
+call CopyProgrunnerWork.bat Pub-16
+call CopyProgrunnerWork.bat Pub-17
 
-call CopyProgrunnerWork.bat \\proto-2\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\proto-3\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\proto-4\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\proto-5\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\proto-6\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\proto-7\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\proto-8\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\proto-9\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\proto-10\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\proto-11\DMS_Programs\MultiProgRunnerSvc\
+echo Update Pub-2*
+rem Offline: Pub-24
+rem Offline: Pub-25
+rem Offline: Pub-26
+rem Offline: Pub-27
+rem Offline: Pub-28
+rem Offline: Pub-29
 
-call CopyProgrunnerWork.bat \\Gigasax\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Diorite\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\EmslMQ01\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\EmslPub01\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Mallard\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Protoapps\DMS_Programs\MultiProgRunnerSvc\
-call CopyProgrunnerWork.bat \\Proteinseqs\DMS_Programs\MultiProgRunnerSvc\
+echo Update Pub-3*
+rem Offline: Pub-30
+rem Offline: Pub-31
+call CopyProgrunnerWork.bat Pub-32
+rem Offline: Pub-33
+call CopyProgrunnerWork.bat Pub-34
+call CopyProgrunnerWork.bat Pub-35
+call CopyProgrunnerWork.bat Pub-36
+call CopyProgrunnerWork.bat Pub-37
+call CopyProgrunnerWork.bat Pub-38
+call CopyProgrunnerWork.bat Pub-39
 
-rem call CopyProgrunnerWork.bat \\seqcluster1\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\seqcluster2\DMS_Programs\MultiProgRunnerSvc\
-rem call CopyProgrunnerWork.bat \\seqcluster5\DMS_Programs\MultiProgRunnerSvc\
+echo Update Pub-4*
+call CopyProgrunnerWork.bat Pub-40
+rem Offline: Pub-41
+call CopyProgrunnerWork.bat Pub-42
+call CopyProgrunnerWork.bat Pub-43
+call CopyProgrunnerWork.bat Pub-44
+call CopyProgrunnerWork.bat Pub-45
+call CopyProgrunnerWork.bat Pub-46
+call CopyProgrunnerWork.bat Pub-47
+call CopyProgrunnerWork.bat Pub-48
+call CopyProgrunnerWork.bat Pub-49
 
+echo Update Pub-5*
+call CopyProgrunnerWork.bat Pub-50
+call CopyProgrunnerWork.bat Pub-51
+call CopyProgrunnerWork.bat Pub-52
+call CopyProgrunnerWork.bat Pub-53
+call CopyProgrunnerWork.bat Pub-54
+call CopyProgrunnerWork.bat Pub-55
+call CopyProgrunnerWork.bat Pub-56
+call CopyProgrunnerWork.bat Pub-57
+call CopyProgrunnerWork.bat Pub-58
+call CopyProgrunnerWork.bat Pub-59
+
+echo Update Pub-6*
+call CopyProgrunnerWork.bat Pub-60
+call CopyProgrunnerWork.bat Pub-61
+call CopyProgrunnerWork.bat Pub-62
+call CopyProgrunnerWork.bat Pub-63
+call CopyProgrunnerWork.bat Pub-64
+call CopyProgrunnerWork.bat Pub-65
+call CopyProgrunnerWork.bat Pub-66
+call CopyProgrunnerWork.bat Pub-67
+call CopyProgrunnerWork.bat Pub-68
+call CopyProgrunnerWork.bat Pub-69
+
+echo Update Pub-7*
+call CopyProgrunnerWork.bat Pub-70
+call CopyProgrunnerWork.bat Pub-71
+call CopyProgrunnerWork.bat Pub-72
+call CopyProgrunnerWork.bat Pub-73
+call CopyProgrunnerWork.bat Pub-74
+call CopyProgrunnerWork.bat Pub-75
+call CopyProgrunnerWork.bat Pub-76
+call CopyProgrunnerWork.bat Pub-77
+call CopyProgrunnerWork.bat Pub-78
+call CopyProgrunnerWork.bat Pub-79
+
+echo Update Pub-8*
+call CopyProgrunnerWork.bat Pub-80
+call CopyProgrunnerWork.bat Pub-81
+call CopyProgrunnerWork.bat Pub-82
+call CopyProgrunnerWork.bat Pub-83
+call CopyProgrunnerWork.bat Pub-84
+call CopyProgrunnerWork.bat Pub-85
+call CopyProgrunnerWork.bat Pub-86
+call CopyProgrunnerWork.bat Pub-87
+call CopyProgrunnerWork.bat Pub-88
+call CopyProgrunnerWork.bat Pub-89
+
+echo Update Pub-9*
+call CopyProgrunnerWork.bat Pub-90
+call CopyProgrunnerWork.bat Pub-91
+call CopyProgrunnerWork.bat Pub-92
+call CopyProgrunnerWork.bat Pub-93
+call CopyProgrunnerWork.bat Pub-94
+call CopyProgrunnerWork.bat Pub-95
+call CopyProgrunnerWork.bat Pub-96
+call CopyProgrunnerWork.bat Pub-97
+
+echo Update Other
+call CopyProgrunnerWork.bat Andesite
+call CopyProgrunnerWork.bat Diorite
+call CopyProgrunnerWork.bat Gigasax
+call CopyProgrunnerWork.bat Granite
+call CopyProgrunnerWork.bat Mallard
+rem Offline: Peaks1
+call CopyProgrunnerWork.bat Peaks2
+call CopyProgrunnerWork.bat Proteinseqs
+call CopyProgrunnerWork.bat Protoapps
+
+rem Pub-1000
+call CopyProgrunnerWork.bat EmslMQ01
+rem Pub-1001
+call CopyProgrunnerWork.bat EmslPub01
+
+rem Offline: seqcluster1
+rem Offline: seqcluster2
+rem Offline: seqcluster5
