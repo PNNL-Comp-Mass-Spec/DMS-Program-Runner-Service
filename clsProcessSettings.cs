dda98d2a86c56c8f278faf088775f9d85de91ca6
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-15 17:47:57 -0700

    Update comments and use latest PRISM.dll

--- clsProcessSettings.cs
+++ clsProcessSettings.cs
@@ -1,16 +1,35 @@
 ﻿
 namespace ProgRunnerSvc
 {
-    class clsProcessSettings
+    internal class clsProcessSettings
     {
 
         protected string m_UniqueKey;
 
+        /// <summary>
+        /// Unique name for this program
+        /// </summary>
         public string UniqueKey => m_UniqueKey;
 
+        /// <summary>
+        /// Path to the program (.exe or .bat) to run
+        /// </summary>
         public string ProgramPath { get; set; }
+
+        /// <summary>
+        /// Arguments to pass to the program
+        /// </summary>
         public string ProgramArguments { get; set; }
+
+        /// <summary>
+        /// Repeat mode
+        /// </summary>
+        /// <remarks>Valid values are Repeat, Once, and No</remarks>
         public string RepeatMode { get; set; }
+
+        /// <summary>
+        /// Holdoff time, in seconds (not milliseconds)
+        /// </summary>
         public int HoldoffSeconds  { get; set; }
 
         /// <summary>
