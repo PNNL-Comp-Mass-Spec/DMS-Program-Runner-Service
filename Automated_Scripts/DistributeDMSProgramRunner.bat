4042a4eab189c124694dfa16c33f3295872b4aed
alchemistmatt <matthew.monroe@pnnl.gov>
2025-10-22 18:36:03 -0700

    Update batch distribution file

--- Automated_Scripts/DistributeDMSProgramRunner.bat
+++ Automated_Scripts/DistributeDMSProgramRunner.bat
@@ -1,14 +1,14 @@
 @echo off
 
 echo About to update the DMS Program Runner (aka Program Runner Service)
-echo by copying to \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DMSProgramRunner
+echo by copying to \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\DMSProgramRunner
 echo Are you sure you want to continue?
 if not "%1"=="NoPause" pause
 
 @echo On
 
-xcopy /d /y ..\bin\ProgRunnerSvc.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DMSProgramRunner\Update\
-xcopy /d /y ..\bin\*.dll             \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DMSProgramRunner\Update\
+xcopy /d /y ..\bin\ProgRunnerSvc.exe \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\DMSProgramRunner\Update\
+xcopy /d /y ..\bin\*.dll             \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\DMSProgramRunner\Update\
 
 @echo off
 if not "%1"=="NoPause" pause
